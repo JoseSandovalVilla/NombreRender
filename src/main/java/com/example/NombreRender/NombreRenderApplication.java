54f480617ebf46131156c69cf09459cbfab5529b
JoseSandovalVilla <jsvilla776@gmail.com>
2024-07-31 08:18:45 -0600

    Devolver nombre

---
+++ src/main/java/com/example/NombreRender/NombreRenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.NombreRender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NombreRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NombreRenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NombreRender/NombreRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NombreRender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NombreRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
